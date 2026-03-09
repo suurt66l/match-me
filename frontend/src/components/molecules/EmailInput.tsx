2c4c6b9848c0f0a40964e2f4f88623f2b1ca8723
Kirill Sergeev <mangil@protonmail.com>
2026-03-09 10:46:39 +0200

    Minor design improvments

--- frontend/src/components/molecules/EmailInput.tsx
+++ frontend/src/components/molecules/EmailInput.tsx
@@ -1,7 +1,7 @@
 export default function EmailInput() {
     return (
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-100">
                 Email address
               </label>
               <div className="mt-2">
@@ -11,7 +11,7 @@ export default function EmailInput() {
                   type="email"
                   required
                   autoComplete="email"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-700 sm:text-sm/6"
                 />
               </div>
             </div>
