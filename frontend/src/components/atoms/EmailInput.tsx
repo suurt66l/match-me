e3b65512bf51a385f75df5799dc78fe26e48298a
Kirill Sergeev <mangil@protonmail.com>
2026-03-09 16:44:56 +0200

    It is possible to sent password to BackEnd

--- frontend/src/components/atoms/EmailInput.tsx
+++ frontend/src/components/atoms/EmailInput.tsx
@@ -1,19 +1,17 @@
 interface Props {
-    email: string,
     setEmail: (value: string) => void;
 }
 
-export default function EmailInput({email, setEmail} : Props) {
+export default function EmailInput({setEmail} : Props) {
     return(
         <input
             id="email"
             name="email"
             type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
             required
             autoComplete="email"
             className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-2 -outline-offset-1 outline-white/25 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-700 sm:text-sm/6"
+            onChange={(event) => setEmail(event.target.value)}
         />
     );
 }
