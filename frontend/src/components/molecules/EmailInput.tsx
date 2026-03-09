12fda069e3511cfda952a480332e83095200d4ef
Kirill Sergeev <mangil@protonmail.com>
2026-03-09 09:46:59 +0200

    Transfered form blocks to molecules

---
+++ frontend/src/components/molecules/EmailInput.tsx
@@ -0,0 +1,19 @@
+export default function EmailInput() {
+    return (
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+    )
+};
---
+++ frontend/src/components/molecules/PasswordInput.tsx
@@ -0,0 +1,26 @@
+export default function PasswordInput() {
+    return (
+        <div>
+            <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+        </div>
+    )
+}
