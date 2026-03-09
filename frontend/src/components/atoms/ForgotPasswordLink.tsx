ce612084d252b5c4e50b0e71a383cd38c3657c72
Kirill Sergeev <mangil@protonmail.com>
2026-03-09 16:33:14 +0200

    Added data sending to server

---
+++ frontend/src/components/atoms/ForgotPasswordLink.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPasswordLink() {
+    return(
+        <a href="#" className="font-semibold text-amber-800 hover:text-amber-700">
+            Forgot password?
+        </a>
+    );
+}
