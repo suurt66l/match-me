c57a136173911f9556df82a062bb48020655f241
Kirill Sergeev <mangil@protonmail.com>
2026-03-10 10:40:01 +0200

    Added sessionStorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,37 @@ import AuthPage from './components/pages/AuthPage';
 import { useState } from 'react';
 import MatcherPage from './components/pages/MatcherPage';
 
-function App() {
-    const [token, setToken] = useState<string | null>(null);
 
-    if(!token) {
-        return <AuthPage setToken={setToken} />
-    }
-    console.log(token)
+function setToken(userToken: string) {
+  //LOGS
+  console.log("Setting token: " + userToken)
+  
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+
+  //LOGS
+  console.log("Setting token: " + tokenString)
+  
+  if (!tokenString) {
+    return null;
+  }
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token;
+}
+
+function App() {
+  const token = getToken();
+
+  if(!token) {
+    return <AuthPage setToken={setToken} />
+  }
+
+  //LOGS
+  console.log("Current token" + token)
+  
   return (
     <div className="wrapper">
       <BrowserRouter>
