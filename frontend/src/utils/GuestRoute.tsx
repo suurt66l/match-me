386508306ee25203c27e8be4df3f86c4aa340cff
Kirill Sergeev <mangil@protonmail.com>
2026-03-11 12:59:46 +0200

    Added GuestRoute to redirect authorized person from public pages

--- frontend/src/utils/GuestRoute.tsx
+++ frontend/src/utils/GuestRoute.tsx
@@ -5,11 +5,11 @@ interface Props {
     children: React.ReactNode;
 }
 
-export default function ProtectedRoute({ children } : Props) {
+export default function GuestRoute({ children } : Props) {
   const { isAuthenticated } = useAuth();
   
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+  if (isAuthenticated) {
+    return <Navigate to="/" replace />;
   }
 
   return children;
