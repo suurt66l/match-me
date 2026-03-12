150d7750494180cf30371443036b618ff8efd93e
Kirill Sergeev <mangil@protonmail.com>
2026-03-12 20:45:49 +0200

    Added all necesarry pages

---
+++ frontend/src/components/pages/ChatPage.tsx
@@ -0,0 +1,11 @@
+export default function ChatPage() {
+    
+    return (
+        <div>
+            { /* Main Zone */}
+            <div className="flex items-center justify-center min-h-screen bg-amber-300">
+                <h1> CHAT PAGE </h1>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/components/pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+export default function ProfilePage() {
+    
+    return (
+        <div>
+            { /* Main Zone */}
+            <div className="flex items-center justify-center min-h-screen bg-amber-300">
+                <h1> PROFILE PAGE </h1>
+            </div>
+        </div>
+    );
+}
