f30729d538b91b568286b695797731bb89a2ed27
Kirill Sergeev <mangil@protonmail.com>
2026-03-12 20:45:49 +0200

    Added all necesarry pages

--- frontend/src/components/atoms/TestLink.tsx
+++
@@ -1,7 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function TestLink() {
-    return(
-        <Link to="/test" className="flex w-full justify-center rounded-md bg-amber-950 px-3 py-1.5 my-2 text-sm/6 font-semibold text-white hover:bg-amber-900"> Test </Link>
-    );
-}
--- frontend/src/components/organisms/NavMenu.tsx
+++ frontend/src/components/organisms/NavMenu.tsx
@@ -1,10 +1,8 @@
-import Button from "../atoms/Button";
 import ChatLink from "../atoms/ChatLink";
 import ConnectionsLink from "../atoms/ConnectionsLink";
 import MatcherLink from "../atoms/MatcherLink";
 import ProfileLink from "../atoms/ProfileLink";
 import SignOutButton from "../atoms/SignOutButton";
-import TestLink from "../atoms/TestLink";
 
 export default function NavMenu () {
     return (
@@ -13,7 +11,6 @@ export default function NavMenu () {
             <ConnectionsLink />
             <ChatLink />
             <ProfileLink />
-            <TestLink />
             <SignOutButton />
         </div>
     );
