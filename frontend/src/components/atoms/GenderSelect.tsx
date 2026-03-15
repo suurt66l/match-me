6fabd933ebeeb4a2aff8e813ab44e31c94bf1d42
Kirill Sergeev <mangil@protonmail.com>
2026-03-15 20:04:19 +0200

    Added Gender Selector

---
+++ frontend/src/components/atoms/GenderSelect.tsx
@@ -0,0 +1,17 @@
+interface Props {
+    setGender: (value: string) => void;
+}
+
+export default function GenderSelect({setGender} : Props) {
+    return(
+        <select id="gender"
+                name="gender"
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-2 -outline-offset-1 outline-white/25 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-700 sm:text-sm/6" 
+                onChange={(event) => setGender(event.target.value) }>
+            <option value="">Select gender:</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="non-binary">Non-binary</option>
+        </select>
+    );
+}
