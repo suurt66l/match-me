5173a09433b6228362571af577b387838872532b
Kirill Sergeev <mangil@protonmail.com>
2026-03-15 20:06:11 +0200

    Added label to Gender

--- frontend/src/components/atoms/GenderSelect.tsx
+++ frontend/src/components/atoms/GenderSelect.tsx
@@ -8,7 +8,7 @@ export default function GenderSelect({setGender} : Props) {
                 name="gender"
                 className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-2 -outline-offset-1 outline-white/25 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-700 sm:text-sm/6" 
                 onChange={(event) => setGender(event.target.value) }>
-            <option value="">Select gender:</option>
+            <option value="">Select gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
             <option value="non-binary">Non-binary</option>
