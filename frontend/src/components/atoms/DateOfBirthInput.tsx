6bfcb24b733c668bfc89b45ddb910627e1914adf
Kirill Sergeev <mangil@protonmail.com>
2026-03-15 19:46:07 +0200

    Added Input for BDay

---
+++ frontend/src/components/atoms/DateOfBirthInput.tsx
@@ -0,0 +1,17 @@
+interface Props {
+    setDateOfBirth: (value: string) => void;
+}
+
+export default function DateOfBirthInput({setDateOfBirth} : Props) {
+    return(
+        <input
+            id="dateOfBirth"
+            name="dateOfBirth"
+            type="date"
+            required
+            autoComplete="bday"
+            className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-2 -outline-offset-1 outline-white/25 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-700 sm:text-sm/6"
+            onChange={(event) => setDateOfBirth(event.target.value)}
+        />
+    );
+}
