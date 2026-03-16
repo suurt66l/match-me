ca29d407e192031af314897e4a457f17502a928f
Kirill Sergeev <mangil@protonmail.com>
2026-03-16 10:31:59 +0200

    Added label for About me input

---
+++ frontend/src/components/atoms/AboutMeLabel.tsx
@@ -0,0 +1,7 @@
+export default function AboutMeLabel() {
+    return(
+        <label htmlFor="about-me" className="block text-sm font-bold text-gray-100">
+            About me:
+        </label>
+    );
+}
