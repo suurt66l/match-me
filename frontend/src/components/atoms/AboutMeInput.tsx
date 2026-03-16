cef4f8bbcc99b76f637d32bda642f29d3754cff9
Kirill Sergeev <mangil@protonmail.com>
2026-03-16 10:31:21 +0200

    Added About me field

---
+++ frontend/src/components/atoms/AboutMeInput.tsx
@@ -0,0 +1,16 @@
+interface Props {
+    setAboutMe: (value: string) => void;
+}
+
+export default function AboutMeInput({setAboutMe} : Props) {
+    return(
+        <textarea
+            id="about-me"
+            name="about-me"
+            required
+            maxLength={2000}
+            className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-2 -outline-offset-1 outline-white/25 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-700 sm:text-sm/6"
+            onChange={(event) => setAboutMe(event.target.value)}
+        />
+    );
+}
