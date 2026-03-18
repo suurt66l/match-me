a7bb385ecb9d36b2cb4843c039cf3395d7ec0c08
Kirill Sergeev <mangil@protonmail.com>
2026-03-18 20:04:30 +0200

    Delete garbage from AuthPage

--- frontend/src/components/pages/AuthPage.tsx
+++ frontend/src/components/pages/AuthPage.tsx
@@ -22,19 +22,12 @@ export default function AuthPage() {
 
     return (
         <div>
-            { /* Navbar */}
-            <div className="flex items-center min-w-screen bg-amber-600">
-                <Logo/>
-            </div>
-            { /* Main Zone */}
-            <div className="flex items-center justify-center min-h-screen bg-amber-300">
             <AuthSection
                 setEmail={setEmail}
                 setPassword={setPassword}
                 error={error}
                 onSubmit={handleSubmit}
             />
-            </div>
         </div>
     );
 }
