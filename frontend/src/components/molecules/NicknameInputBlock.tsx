bfc5c8e0e72a1b66b65a7ba7919cc57e83912394
Kirill Sergeev <mangil@protonmail.com>
2026-03-23 15:06:29 +0200

    Fixed input Nickname

--- frontend/src/components/molecules/NicknameInputBlock.tsx
+++ frontend/src/components/molecules/NicknameInputBlock.tsx
@@ -2,17 +2,15 @@ import NicknameInput from "../atoms/NicknameInput";
 import NicknameLabel from "../atoms/NicknameLabel";
 interface Props {
   setNickname: (value: string) => void;
-  value: string;
 }
 
-export default function NicknameInputBlock({setNickname, value} : Props) {
+export default function NicknameInputBlock({setNickname} : Props) {
     return (
             <div>
               <NicknameLabel />
               <div className="mt-2">
                 <NicknameInput
                   setNickname={setNickname}
-                  value={value}
                 />
               </div>
             </div>
