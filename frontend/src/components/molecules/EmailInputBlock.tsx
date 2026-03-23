033ecd7da18360ee430ea4c17fbdf2fa419c5e9f
Kirill Sergeev <mangil@protonmail.com>
2026-03-23 18:21:38 +0200

    Added some refact

--- frontend/src/components/molecules/EmailInputBlock.tsx
+++ frontend/src/components/molecules/EmailInputBlock.tsx
@@ -1,15 +1,17 @@
 import EmailInput from "../atoms/EmailInput"
 import EmailLabel from "../atoms/EmailLabel"
 interface Props {
+  value: string;
   setEmail: (value: string) => void;
 }
 
-export default function EmailInputBlock({setEmail} : Props) {
+export default function EmailInputBlock({value, setEmail} : Props) {
     return (
             <div>
               <EmailLabel />
               <div className="mt-2">
                 <EmailInput
+                  value={value}
                   setEmail={setEmail}
                 />
               </div>
--- frontend/src/components/molecules/NicknameInputBlock.tsx
+++ frontend/src/components/molecules/NicknameInputBlock.tsx
@@ -1,15 +1,17 @@
 import NicknameInput from "../atoms/NicknameInput";
 import NicknameLabel from "../atoms/NicknameLabel";
 interface Props {
+  value: string;
   setNickname: (value: string) => void;
 }
 
-export default function NicknameInputBlock({setNickname} : Props) {
+export default function NicknameInputBlock({value, setNickname} : Props) {
     return (
             <div>
               <NicknameLabel />
               <div className="mt-2">
                 <NicknameInput
+                  value={value}
                   setNickname={setNickname}
                 />
               </div>
