ad1f7b66ea9ba2e7ba2d0ead69a286b45cd80523
Kirill Sergeev <mangil@protonmail.com>
2026-03-31 10:55:01 +0300

    Replacesd countries with continents and made contitnets with gender SingleSelector

--- frontend/src/components/atoms/ContinentSelect.tsx
+++ frontend/src/components/atoms/ContinentSelect.tsx
@@ -1,13 +1,13 @@
 import CustomSelect from "./CustomSelect";
-import { countries } from "../../data/countries";
+import { CONTINENTS } from "../../data/continents";
 
 interface Props {
   setCountry: (value: string) => void;
   value: string;
 }
 
-const options = countries.map(c => ({ value: c, label: c }));
+const options = CONTINENTS.map(c => ({ value: c, label: c }));
 
-export default function CountrySelect({ setCountry, value }: Props) {
+export default function ContinentSelect({ setCountry, value }: Props) {
   return <CustomSelect options={options} value={value} setValue={setCountry} placeholder="Select country" />;
 }
---
+++ frontend/src/data/continents.ts
@@ -0,0 +1,9 @@
+export const CONTINENTS = [
+  "Africa",
+  "Antarctica",
+  "Asia",
+  "Europe",
+  "North America",
+  "Oceania",
+  "South America"
+];
